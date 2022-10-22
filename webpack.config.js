1b44162cba37a3c52a35b974f9a98ad5f4ceea9a
youthfulhps <ybh942002@gmail.com>
2022-10-22 22:44:38 +0900

    Setting: Modified webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,57 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
 
 module.exports = {
-  mode: isProd ? "production" : "development",
-  devtool: isProd ? "hidden-source-map" : "source-map",
-  entry: "./src/index.tsx",
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? 'hidden-source-map' : 'source-map',
+  entry: './src/index.tsx',
   output: {
-    filename: "[name].js",
-    path: path.join(__dirname, "/dist")
+    filename: '[name].js',
+    path: path.join(__dirname, '/dist')
   },
   resolve: {
-    modules: ["node_modules"],
-    extensions: [".js", ".jsx", ".ts", ".tsx"]
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         options: {
           transpileOnly: !isProd
         }
       },
       {
         test: /\.css?$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(webp|jpg|png|jpeg)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[ext]?[hash]"
+          name: '[name].[ext]?[hash]'
         }
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-      hash: true
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      hash: false
     })
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, "public"),
-    host: "localhost",
+    host: 'localhost',
     port: PORT,
     open: true,
     hot: true,
     compress: true,
-    historyApiFallback: true,
-    overlay: true,
-    stats: "errors-only"
+    historyApiFallback: true
   }
 };
